810783a94424dca80b4866670ce7b6f4b7e956e0
Meghrig Terzian <76222303+meghrigJAC@users.noreply.github.com>
2024-01-26 18:00:31 -0500

    Comments added

--- Program.cs
+++ Program.cs
@@ -2,23 +2,31 @@
 {
     internal class Program
     {
+        /*	Program name: Programming 2: Debugging Lecture
+       Created by: Meghrig Terzian 
+       Date:26/01/2024
+       Code for Lab 2 to Reinforce debugging */
         static void Main(string[] args)
         {
+
             int[] numbers = new int[] { 6, 4, 3, 1, 7, 2, 10 };
 
-            //Getting the 3 smallest numbers
+            //Calling GetSmallests to get the 3 smallest numbers
 
             int[] smallests = GetSmallests(numbers, 3);
+
+            //Printing the smallest elements 
 
             PrintArray("Smallest numbers:", smallests);
 
         }
 
+        // GetSmallests takes an integer array and an integer and returns an integer array with the count smallest elements
         static int[] GetSmallests(int[] array, int count)
         {
             int[] smallests = new int[count];
 
-            for(int i=0;i< count; i++)
+            for (int i = 0; i < count; i++)
             {
                 int minIndex = GetSmallestIndex(array);
                 smallests[i] = array[minIndex];
@@ -29,6 +37,7 @@
 
         }
 
+        // GetSmallestIndex takes an integer array and returns the index of the smallest element
         static int GetSmallestIndex(int[] array)
         {
             //Assume the first element is the smallest
@@ -42,10 +51,11 @@
             return minIndex;
         }
 
+        // PrintArray takes a string and an int array and prints the string and the array elements
         static void PrintArray(string listName, int[] array)
         {
-           
-            for (int i=0; i<array.Length;i++)
+
+            for (int i = 0; i < array.Length; i++)
                 Console.WriteLine(array[i]);
 
         }
